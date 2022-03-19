13dd1d655d8d1ba495889ab25dd889bb55ff62e5
Izaak Brown <izaakbthunderbird@gmail.com>
2022-03-19 15:22:32 -0400

    fix service account path

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,7 +3,7 @@ import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
 initializeApp({
-  credential: cert(join(__dirname, './service-account.json'))
+  credential: cert(join(__dirname, '../service-account.json'))
 });
 
 export const db = getFirestore();
