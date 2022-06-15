97c87298965196d7470f3ff0a68db69d602ee521
Izaak Brown <izaakbthunderbird@gmail.com>
2022-06-15 16:24:14 -0400

    fix 8ball

--- irohoshi/main.ts
+++ irohoshi/main.ts
@@ -5,14 +5,18 @@ import { Command, Commands } from './commands/command.ts';
 
 init({ env: true });
 
-Object.entries(
-  commands as unknown as Record<string, Command | Commands>
-).forEach(([name, commandOrCommands]) =>
-  typeof commandOrCommands.desc === 'string'
-    ? run(name, commandOrCommands as Command)
-    : Object.entries(commandOrCommands as Commands).forEach(
-        ([subName, command]) => run(`${name} ${subName}`, command)
-      )
+const { default: oddNameCommands, ...normalCommands } = commands;
+
+Object.entries({
+  ...normalCommands,
+  ...oddNameCommands
+} as unknown as Record<string, Command | Commands>).forEach(
+  ([name, commandOrCommands]) =>
+    typeof commandOrCommands.desc === 'string'
+      ? run(name, commandOrCommands as Command)
+      : Object.entries(commandOrCommands as Commands).forEach(
+          ([subName, command]) => run(`${name} ${subName}`, command)
+        )
 );
 function run(name: string, { options, handler }: Command) {
   handle(name, i =>
