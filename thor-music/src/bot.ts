b034654b56dc2e4e947d3c4e08e87dab749f4583
Izaak Brown <izaakbthunderbird@gmail.com>
2022-06-18 14:33:56 -0400

    fix thor-music starting

---
+++ thor-music/src/bot.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-cycle */
+import './env';
+import DiscordBot from '$shared/bot';
+import commands from './commands';
+import help from './commands/help';
+import players from './players';
+
+const bot = new DiscordBot(
+  'Thor Music',
+  '-',
+  ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES', 'DIRECT_MESSAGES'],
+  process.env.TOKEN
+).addCommands([help, ...commands]);
+bot.client.on('voiceStateUpdate', oldState => {
+  if (
+    oldState.channel?.members.has(process.env.DISCORD_BOT_ID || '') &&
+    oldState.channel?.members.size === 1
+  ) {
+    const guildId = oldState.guild.id;
+    const player = players.get(guildId);
+    player?.stop();
+  }
+});
+export default bot;
