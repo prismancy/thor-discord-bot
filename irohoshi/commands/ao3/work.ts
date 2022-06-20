7d6531167c858a927a39fc2d51c48bb1ef90c0cf
Izaak Brown <izaakbthunderbird@gmail.com>
2022-06-20 00:15:05 -0400

    in5net.dev > in5.network

--- irohoshi/commands/ao3/work.ts
+++ irohoshi/commands/ao3/work.ts
@@ -44,7 +44,7 @@ export default command(
         .setAuthor({ name: author, url: authorURL, icon_url: iconURL })
         .setURL(workUrl)
         .setThumbnail(
-          `https://files.in5net.dev/ao3/squares/${symbols.rating}_${symbols.orientation}_${symbols.warning}_${symbols.complete}.png`
+          `https://files.in5.network/ao3/squares/${symbols.rating}_${symbols.orientation}_${symbols.warning}_${symbols.complete}.png`
         );
       if (rating) embed.addField('Rating', ratings[rating]);
       if (warnings?.length)
