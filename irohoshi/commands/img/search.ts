47cd509c501a220db86c50829b7d4ac4179feebe
Izaak Brown <izaakbthunderbird@gmail.com>
2022-06-27 18:49:40 -0400

    more advanced file_name search

--- irohoshi/commands/img/search.ts
+++ irohoshi/commands/img/search.ts
@@ -5,28 +5,28 @@ export default command(
   {
     desc: 'Search for an image from yyyyyyy.info',
     options: {
-      query: {
+      file_name: {
         type: 'string',
-        desc: 'Query to search for',
+        desc: 'THe file name to search for',
         autocomplete: async query => {
           const { data } = await imagesTable()
             .select('file_name')
-            .textSearch('file_name_search', `${query}:*`)
+            .textSearch(
+              'file_name_search',
+              query
+                .split(' ')
+                .filter(Boolean)
+                .map(word => `${word}:*`)
+                .join(' & ')
+            )
             .limit(5);
-          console.log(data);
           return data?.map(({ file_name }) => file_name) || [];
         }
       }
     }
   },
-  async i => {
-    const { data } = await imagesTable()
-      .select('file_name')
-      .eq('file_name', '*')
-      .limit(1)
-      .single();
-    const filename = data?.file_name || '';
-    const url = `${Deno.env.get('FILES_ORIGIN')}/y7/images/${filename}`;
+  (i, { file_name }) => {
+    const url = `${Deno.env.get('FILES_ORIGIN')}/y7/images/${file_name}`;
     return i.reply(url);
   }
 );
