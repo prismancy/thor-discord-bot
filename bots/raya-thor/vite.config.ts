f81f74eef2f25cc5f62ecb92893d17a74d220a0a
Izaak Brown <izaakbthunderbird@gmail.com>
2023-05-21 03:41:38 -0400

    test custom text model

--- bots/raya-thor/vite.config.ts
+++ bots/raya-thor/vite.config.ts
@@ -22,6 +22,13 @@ export default defineConfig({
     }
   },
   build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      fileName: 'main',
+      formats: ['es']
+    },
+    outDir: 'build',
+    sourcemap: true,
     rollupOptions: {
       external
     }
