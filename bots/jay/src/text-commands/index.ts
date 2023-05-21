578894205341b785c5b3bd8fac028bb9a7cd9090
Izaak Brown <izaakbthunderbird@gmail.com>
2023-05-21 02:30:35 -0400

    test custom text model

--- bots/jay/src/text-commands/index.ts
+++ bots/jay/src/text-commands/index.ts
@@ -1,5 +1,6 @@
 import type { TextCommand } from '$services/commands/text';
 import glsl from './glsl';
+import ryu from './ryu';
 
-const commands = { glsl } as unknown as Record<string, TextCommand>;
+const commands = { glsl, ryu } as unknown as Record<string, TextCommand>;
 export default commands;
