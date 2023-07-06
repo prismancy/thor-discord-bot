0a73dd3d2c5e1b350243b2e4f15e651372b80d71
Izaak Brown <izaakbthunderbird@gmail.com>
2023-07-05 23:01:33 -0400

    test: test

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -4,7 +4,7 @@ import { join } from "node:path";
 import pino, { type TransportTargetOptions } from "pino";
 
 const prod = env.NODE_ENV === "production";
-console.log("production:", prod);
+console.log("env:", env);
 
 const logsPath = new URL("../../logs", import.meta.url).pathname;
 if (!existsSync(logsPath)) mkdirSync(logsPath);
