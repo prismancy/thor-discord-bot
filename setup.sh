93e1db144b639be9fc7fc2505b7d5174de71bb37
Izaak Brown <izaakbthunderbird@gmail.com>
2023-07-17 21:30:32 -0400

    chore(deps): update

--- setup.sh
+++ setup.sh
@@ -3,11 +3,6 @@ sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y
 
 sudo apt install curl git ffmpeg -y
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-
-cargo binstall zellij
-
 git config --global credential.helper store
 git config --global pull.rebase false
 
@@ -18,4 +13,8 @@ cd discord-bots
 fnm use
 
 curl -fsSL https://get.pnpm.io/install.sh | sh -
+
 pnpm i --frozen-lockfile --prod
+
+pnpm i -g pm2
+pm2 start "pnpm run start:raya" --wait-ready --listen-timeout 60000
