247f6b7da0393a9bfed42ae6f1a498749b04d6f9
Izaak Brown <48165199+in5net@users.noreply.github.com>
2023-09-24 12:33:30 -0400

    feat(commit): use gum instead of commitlint

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+TYPE=$(gum choose "feat" "fix" "style" "docs" "refactor" "perf" "chore" "revert" "build" "test")
+SCOPE=$(gum input --placeholder "scope")
+
+# Since the scope is optional, wrap it in parentheses if it has a value.
+test -n "$SCOPE" && SCOPE="($SCOPE)"
+
+# Pre-populate the input with the type(scope): so that the user may change it
+SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
+
+# Commit these changes
+gum confirm "Commit changes?" && git commit -m "$SUMMARY"
