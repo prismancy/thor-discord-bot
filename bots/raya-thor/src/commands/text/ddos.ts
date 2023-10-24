738f1d94a1016652bba4cf615dee3d942a52110c
Izaak Brown <48165199+in5net@users.noreply.github.com>
2023-10-23 21:41:53 -0400

    feat(cmd): ddos silliness

---
+++ bots/raya-thor/src/commands/text/ddos.ts
@@ -0,0 +1,31 @@
+import { sleep } from "@in5net/std/async";
+import { random, randomInt } from "@in5net/std/random";
+import command from "discord/commands/text";
+
+export default command(
+	{
+		desc: "Launches a DDOS attack",
+		args: {},
+	},
+	async ({ message: { channel } }) => {
+		await channel.sendTyping();
+		await sleep(random(5000));
+		await channel.send("Launching DDOS attack...");
+		await sleep(random(5000));
+		await channel.send("Preparing fleet...");
+		await sleep(random(5000));
+		await channel.send(`Fleet size chosen: ${randomInt(100, 1000)}`);
+		await sleep(random(5000));
+		await channel.send("Launching attack...");
+		await sleep(random(5000));
+		await channel.send("Attack launched!");
+		await sleep(random(5000));
+		await channel.send(
+			`Attack finished, ${randomInt(100)}% packet loss, ${randomInt(
+				100,
+			)}% packet delay, ${randomInt(100)}% packet corruption, ping: ${randomInt(
+				1000,
+			)}ms, casualties: ${randomInt(100, 1000)}, end of log.`,
+		);
+	},
+);
