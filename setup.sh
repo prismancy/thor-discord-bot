e6d54a2be7a9ba7ef5b26e8bf38e8be76060f7a2
Izaak Brown <48165199+in5net@users.noreply.github.com>
2023-12-04 01:46:30 -0500

    chore(deps): update

--- setup.sh
+++ setup.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 sudo apt update; sudo apt upgrade -y; sudo apt autoremove -y
 
-sudo apt install curl git ffmpeg -y
+sudo apt install build-essential git ffmpeg -y
 
 git config --global credential.helper store
 git config --global pull.rebase false
 
-git clone https://github.com/in5net/discord-bots.git
+git clone https://in5net@github.com/in5net/discord-bots.git
 gsutil -m cp -r "gs://in5net-vm-transfer/discord-bots" .
 cd discord-bots
 
 fnm use
 
-curl -fsSL https://get.pnpm.io/install.sh | sh -
+corepack enable
+corepack prepare pnpm@latest --activate
 
-pnpm i --frozen-lockfile --prod
+pnpm i --prod
 
 pnpm i -g pm2
-pm2 start "pnpm run start:raya" --wait-ready --listen-timeout 60000
+pm2 start "pnpm run start:raya"
