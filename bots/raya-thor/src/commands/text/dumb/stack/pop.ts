5d0b2778b4d5f24aec361baccdd84ff11d65392b
Izaak Brown <48165199+in5net@users.noreply.github.com>
2024-01-01 03:05:42 -0500

    Add stack push and pop commands

---
+++ bots/raya-thor/src/commands/text/dumb/stack/pop.ts
@@ -0,0 +1,32 @@
+import { cache } from "$services/prisma";
+import { time } from "discord.js";
+import command from "discord/commands/text";
+
+export default command(
+	{
+		desc: "Removes the last an item from the stack",
+		args: {},
+	},
+	async () => {
+		const item = await cache.stackItem.findFirst({
+			select: {
+				id: true,
+				createdAt: true,
+				value: true,
+			},
+			orderBy: {
+				id: "desc",
+			},
+		});
+		if (!item) return "Nothing was popped since the stack is empty";
+		await cache.stackItem.delete({
+			where: {
+				id: item.id,
+			},
+		});
+		const count = await cache.stackItem.count();
+		return `Item popped! New length: ${count}
+Pushed at: ${time(item.createdAt)}
+Value: ${item.id}`;
+	},
+);
---
+++ bots/raya-thor/src/commands/text/dumb/stack/push.ts
@@ -0,0 +1,23 @@
+import { cache } from "$services/prisma";
+import command from "discord/commands/text";
+
+export default command(
+	{
+		desc: "Add an item to the stack",
+		args: {
+			value: {
+				type: "text",
+				desc: "The item to push",
+			},
+		},
+	},
+	async ({ args: { value } }) => {
+		await cache.stackItem.create({
+			data: {
+				value,
+			},
+		});
+		const count = await cache.stackItem.count();
+		return `Item pushed! New length: ${count}`;
+	},
+);
