fad53bfe7977d005fbc0af441bbe8528126ef1d4
iz7n <48165199+iz7n@users.noreply.github.com>
2024-05-27 04:18:27 -0400

    fix(schema): use bigint for sqlite numerics

--- packages/database/drizzle.ts
+++ packages/database/drizzle.ts
@@ -6,6 +6,7 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import { env } from "node:process";
 
 const sqlite = new Database(env.DATABASE_URL);
+sqlite.defaultSafeIntegers();
 const db = drizzle(sqlite, { schema });
 export default db;
 
