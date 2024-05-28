6ae4a9a7856a432d0692d58ed1ceb92217204ab7
iz7n <48165199+iz7n@users.noreply.github.com>
2024-05-28 04:24:59 -0400

    chore: remove better-sqlite bigint

--- packages/database/drizzle.ts
+++ packages/database/drizzle.ts
@@ -6,7 +6,6 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import { env } from "node:process";
 
 const sqlite = new Database(env.DATABASE_URL);
-sqlite.defaultSafeIntegers();
 const db = drizzle(sqlite, { schema });
 export default db;
 
