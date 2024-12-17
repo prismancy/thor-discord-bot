7bf42b1fcece05ecb5aa05cbcf43cf95e98008fb
iz7n <48165199+iz7n@users.noreply.github.com>
2024-12-17 00:30:54 -0500

    feat(chat): show starting & running messages

--- src/commands/text/ai/chat.ts
+++ src/commands/text/ai/chat.ts
@@ -4,7 +4,6 @@ import command from "$lib/discord/commands/text";
 import logger from "$lib/logger";
 import { throttle } from "@iz7n/std/async";
 import { ttlCache } from "@iz7n/std/fn";
-import { type Message } from "discord.js";
 import ms from "ms";
 import { readFile } from "node:fs/promises";
 import ollama from "ollama";
@@ -64,17 +63,13 @@ export default command(
     });
 
     let reply = "";
-    let responseMessage: Message | undefined;
+    logger.info(`Starting ${model}...`);
+    const responseMessage = await channel.send("*Starting...*");
     const send = throttle(async () => {
       if (reply) {
-        if (responseMessage) {
-          await responseMessage.edit(reply);
-        } else {
-          responseMessage = await channel.send(reply);
-        }
+        await responseMessage.edit(reply);
       }
     }, 3000);
-    logger.info(`Starting ${model}...`);
     const start = performance.now();
     const response = await ollama.chat({
       model,
@@ -99,6 +94,7 @@ export default command(
       stream: true,
     });
     logger.info(`Running ${model}...`);
+    await responseMessage.edit("*Running...*");
     for await (const part of response) {
       reply += part.message.content;
       send();
