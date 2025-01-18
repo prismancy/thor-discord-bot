e3d448f13cd30a0810c32c6ec9a47671e07035f8
iz7n <48165199+iz7n@users.noreply.github.com>
2025-01-18 18:02:31 -0500

    fix(dash): default to dark mode

--- src/routes/(app)/dark-mode.ts
+++ src/routes/(app)/dark-mode.ts
@@ -1,7 +1,7 @@
 import { browser } from "$app/environment";
 import { writable } from "svelte/store";
 
-const initialMode = browser ? localStorage.getItem("theme") : "light";
+const initialMode = browser ? localStorage.getItem("theme") : "dark";
 export const darkMode = writable(initialMode === "dark");
 darkMode.subscribe($darkMode => {
   const theme = $darkMode ? "dark" : "light";
