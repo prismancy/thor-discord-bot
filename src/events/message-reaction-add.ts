939422d97ee0dd455b55b5ff052b8f71e9aa6e6a
iz7n <48165199+iz7n@users.noreply.github.com>
2025-02-05 04:27:08 -0500

    feat: bookmarks

--- src/events/message-reaction-add.ts
+++ src/events/message-reaction-add.ts
@@ -3,6 +3,8 @@ import { youtubeSearches } from "$lib/database/schema";
 import event from "$lib/discord/event";
 import { YouTubeSong } from "$src/music/songs";
 import { getVoice } from "$src/music/voice-manager";
+import { includesAny } from "@iz7n/std/array";
+import { EmbedBuilder, hyperlink, messageLink } from "discord.js";
 import { env } from "node:process";
 
 const numberEmojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
@@ -10,11 +12,14 @@ const numberEmojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
 export default event(
   { name: "messageReactionAdd" },
   async ({ args: [{ message, emoji }, user] }) => {
+    console.log(user);
     if (user.bot) {
       return;
     }
 
-    const { guild, channelId } = message;
+    console.log(user.dmChannel, emoji.name);
+
+    const { guild, channel, channelId, author, attachments } = message;
     if (guild) {
       const numberIndex = numberEmojis.indexOf(emoji.name || "");
       if (numberIndex !== -1) {
@@ -37,15 +42,69 @@ export default event(
               message = await message.fetch();
             }
             voice.setChannels(message);
-            const song = await YouTubeSong.fromId(id, {
+            const song = await YouTubeSong.fromId(id);
+            song.requester = {
               uid: user.id,
               name: member.displayName,
-            });
+            };
             voice.queue.push(song);
             await voice.play();
           }
         }
+      } else if (emoji.name === "🔖" && user.dmChannel) {
+        const embed = new EmbedBuilder();
+        if (author) {
+          embed.setTitle(author.displayName).setAuthor({
+            name: author.displayName,
+            iconURL: author.defaultAvatarURL,
+          });
+        }
+        embed
+          .setTitle(`Bookmark from ${guild.name}`)
+          .setDescription(message.content)
+          .setTimestamp(message.createdAt);
+        if (attachments.size) {
+          const firstAttactment = attachments.first();
+          if (
+            attachments.size === 1 &&
+            firstAttactment &&
+            includesAny(
+              [firstAttactment.name.split(".").at(-1)],
+              ["png", "jpg", "webp"],
+            )
+          ) {
+            embed.setImage(firstAttactment.proxyURL);
+          } else {
+            embed.addFields({
+              name: "Attachments",
+              value: [...attachments.values()]
+                .map(x => hyperlink(x.name, x.proxyURL))
+                .join("\n"),
+            });
+          }
+        }
+        embed.addFields({
+          name: "Source",
+          value: hyperlink(
+            "[Jump to message]",
+            messageLink(channelId, message.id, guild.id),
+          ),
+        });
+
+        const bookmarkMessage = await user.dmChannel.send({
+          embeds: [embed],
+        });
+        await bookmarkMessage.pin();
+        await bookmarkMessage.react("❌");
       }
+    }
+
+    if (
+      channel.isDMBased() &&
+      emoji.name === "❌" &&
+      message.author?.id === env.DISCORD_ID
+    ) {
+      await message.delete();
     }
 
     if (user.id === env.OWNER_ID) {
